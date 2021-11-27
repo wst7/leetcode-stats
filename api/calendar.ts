864558cd960be7012849d6ec5bc4824615e3e625
wstreet <wstreet7@outlook.com>
2021-11-27 23:19:20 +0800

    vercel build

--- api/calendar.ts
+++ api/calendar.ts
@@ -3,14 +3,15 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import fetcher from "../fetchers/commit-fetcher";
 import Card from "../cards/commit-card";
 
-export default async function (req: VercelRequest, res: VercelResponse) {
+export default function (req: VercelRequest, res: VercelResponse) {
   try {
     const { username, theme } = req.query;
-    const data = await fetcher(username as string);
-    const card = new Card(data, theme as any);
-    res.setHeader("Content-Type", "image/svg+xml");
-    res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
-    res.status(200).send(card.render());
+    fetcher(username as string).then((data) => {
+      const card = new Card(data, theme as any);
+      res.setHeader("Content-Type", "image/svg+xml");
+      res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
+      res.status(200).send(card.render());
+    });
   } catch (error) {
     res.status(400).send("Error");
   }

2dce0e92fbbaa6e65c90f7efcf5b0ffdd427cb17
wstreet <wstreet7@outlook.com>
2021-11-27 22:41:53 +0800

    feat: edit vercel config file

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,21 +5,23 @@ import Card from "../cards/commit-card";
 const app = express();
 const port = 3000;
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/api/ping", (req, res) => {
-  res.send("pong");
-});
-
 app.get("/api/calendar", async (req, res) => {
-  const { username, theme } = req.query;
-  const data = await fetcher(username as string);
-  const card = new Card(data, theme as any);
-  res.send(card.render());
+  try {
+    const { username, theme } = req.query;
+    const data = await fetcher(username as string);
+    const card = new Card(data, theme as any);
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
+    res.status(200).send(card.render());
+  } catch (error) {
+    res.status(400).send('Error');
+  }
+ 
 });
-
 
 app.listen(port, () => {
   console.log(`leetcode-stats listening at http://localhost:${port}`);

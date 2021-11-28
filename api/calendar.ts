0c3e0d9a91136d60c359b2f3106b0c2cbf1ae953
wstreet <wstreet7@outlook.com>
2021-11-28 16:34:11 +0800

    add console

--- api/calendar.ts
+++ api/calendar.ts
@@ -7,7 +7,9 @@ export default function (req: VercelRequest, res: VercelResponse) {
   try {
     const { username, theme } = req.query;
     fetcher(username as string).then((data) => {
+      console.log(data)
       const card = new Card(data, theme as any);
+      console.log(card)
       res.setHeader("Content-Type", "image/svg+xml");
       res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
       res.status(200).send(card.render());

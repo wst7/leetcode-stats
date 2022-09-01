b09e57e0cf3db4514eca12be8ff8cd22f0f80b10
wstreet <wstreet7@outlook.com>
2022-09-01 18:24:07 +0800

    debug console
    Change-Id: I8bd2a448983316814a7f2889a7b461b69e88e9f8

--- api/calendar.ts
+++ api/calendar.ts
@@ -3,19 +3,19 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import fetcher from "../src/fetchers/commit-fetcher";
 import Card from "../src/cards/commit-card";
 
-export default function (req: VercelRequest, res: VercelResponse) {
+export default async function (req: VercelRequest, res: VercelResponse) {
   try {
     const { username, theme } = req.query;
     if (!username) throw Error("Invalid username");
 
-    fetcher(username as string).then((data) => {
-      console.log(data)
-      const card = new Card(data, theme as any);
-      const svg = card.render();
-      res.setHeader("Content-Type", "image/svg+xml");
-      res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
-      res.status(200).send(svg);
-    });
+    const data = await fetcher(username as string);
+    console.log(data);
+    const card = new Card(data, theme as any);
+    const svg = await card.render();
+    console.log(svg);
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
+    res.status(200).send(svg);
   } catch (error) {
     res.status(400).send(error.message);
   }
